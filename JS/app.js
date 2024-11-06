a9e418f573ba0dfed067ff0b41848f0e343ed44b
Franco Monti Venesia <francovenesia05@gmail.com>
2024-11-06 20:00:36 -0300

    Creacón de carrito de compras

---
+++ JS/app.js
@@ -0,0 +1,29 @@
+const producto = prompt("Ingresa el nombre del producto para ver el precio o escribe 'SALIR' para finalizar.");
+
+debugger;
+while (producto !== "SALIR") {
+    switch (producto) {
+        case "Tallarines":
+            alert("El precio por kg de los Tallarines es $3700.");
+            break;
+        case "Ñoquis":
+            alert("El precio por kg de los Ñoquis es $3800.");
+            break;
+        case "Ravioles":
+            alert("El precio por kg de los Ravioles es $7500.");
+            break;
+        case "Tirabuzones":
+            alert("El precio por kg de los Tirabuzones es $3700.");
+            break;
+        case "Sorrentinos":
+            alert("El precio por kg de los Sorrentinos es $8500.");
+            break;
+        case "Canelones":
+            alert("El precio por kg de los Canelones es $8500.");
+            break;
+        default:
+            alert("No tenemos ese producto en el carrito.");
+            break;
+    }
+    producto = prompt("Ingresa otro producto para ver el precio o escribe 'SALIR' para finalizar.");
+}
